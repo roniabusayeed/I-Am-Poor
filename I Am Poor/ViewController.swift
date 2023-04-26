bb8ac9209ae49f7820f0fa46992efa82776dfb59
Abu Sayeed Roni <roni.abusayeed@dal.ca>
2023-04-26 20:52:59 -0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Abu Sayeed Roni on 2023-04-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
